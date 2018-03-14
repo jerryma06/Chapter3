120a5189e47f06bd9e7ac29dbd9d215c0e329465
Bigdata <zeyu.ma@hotmail.com>
2018-03-14 14:57:20 -0700

    Create exercise_3_2.c

---
+++ exercise_3_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	float x, y;
+	
+	printf ("Enter x, j, y value: ");
+	scanf ("%f%d%f", &x, &i, &y);
+	
+	printf ("%f,%d,%f", x, i, y);
+	
+	
+	return 0;
+
+
+/* if input 12.3		45.6	789 , then x = 12.3, i = 45, and y = 0.6*/
+
+}
